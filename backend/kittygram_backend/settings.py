d793347167b53e8408557489ec412117f057cf2b
Олег Дядьков <dyadkov@gmail.com>
2023-06-10 09:14:53 +0500

    Секретный ключ Джанго в пайплайне

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
